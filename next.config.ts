d898608f730f8010f1479a525b3da047331f0fce
Chad Walker <chad@cwalker.dev>
2026-01-19 17:07:21 -0600

    unoptimized images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    loader: 'default',
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
